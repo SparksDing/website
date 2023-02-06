3578839d6458fc6504925f6798270c8c40016d4e
liho21ia <liho21ia@student.ju.se>
2023-02-06 11:04:38 +0800

    fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const dummyData = require('./dummy-data')
 const CrudAPI = require("./crud").CrudAPI
 const express = require('express')
+var request = require('request');
 const multer = require("multer")
 const expressHandlebars = require('express-handlebars')
 const bcrypt = require('bcrypt');
@@ -8,30 +8,8 @@ const redisSrv = require("./redis-server")
 const redis = require("redis")
 const session = require("express-session")
 var fs = require('fs');
-// var upload = multer({ 
-//   dest: 'public/resource', 
-//   filename(req, file, cb) {
-//     console.log(file)
-//     const filenameArr = file.originalname.split('.');
-//     cb(null, filenameArr[filenameArr.length-1]);
-//   } 
-// })
-
-
-const storage = multer.diskStorage({
-  // destination:'public/uploads/'+new Date().getFullYear() + (new Date().getMonth()+1) + new Date().getDate(),
-  destination(req,res,cb){
-    cb(null, 'public/resource/')
-  },
-  filename(req,file,cb){
-    //console.log(file)
-    const filenameArr = file.originalname;
-    cb(null, filenameArr);
-  }
-})
-
-
-var upload = multer({ storage: storage })
+var https = require('https');
+var bodyParser = require('body-parser')
 
 let RedisStore = require("./")(session)
 
@@ -41,9 +19,10 @@ let RedisStore = require("./")(session)
 //   "port": 6379
 // }
 
-
 const app = express()
 app.set('view engine', 'jade');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static("public"))
 app.use(express.urlencoded( {extended: false} ))
 app.use(session({
@@ -55,516 +34,10 @@ app.use(session({
 
 
 // var client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true})
-var client = redis.createClient(process.env.REDISCLOUD_URL);
+var client = redis.createClient("redis://localhost:6379");
 // var client = redis.createClient(redis_config)
 
-app.engine("hbs", expressHandlebars.engine({
-  defaultLayout: 'main.hbs'
-}))
 
-app.get('/', function(request, response){
-  response.redirect("/home")
-})
-
-app.get('/home', function(request, response) {
-  var store = new RedisStore({ client })
-  store.get(request.sessionID, (err, result_store) => {
-      // console.log(result)
-      if(result_store == null) {
-        const model = {
-          Home: true,
-          login_status: false
-        }
-        response.render("index.hbs", model)
-      } else {
-        const model = {
-          Home: true,
-          login_status: true
-        }
-        response.render("index.hbs", model)
-      }
-    })
-  })
-
-app.get('/blog_operations', function(request, response) {
-  response.render("blog_operation.hbs")
-})
-
-function transformTime(timestamp) {
-  if (timestamp) {
-      var time = new Date(timestamp);
-      var y = time.getFullYear();
-      var M = time.getMonth() + 1; 
-      var d = time.getDate(); 
-      var h = time.getHours(); 
-      var m = time.getMinutes(); 
-      var s = time.getSeconds(); 
-      return y + '-' + M + '-' + d + ' ' + h + ':' + m + ':' + s;
-    } else {
-        return '';
-    }
-}
-
-app.get('/portfolio', function(request, response) {
-  var store = new RedisStore({ client })
-  store.get(request.sessionID, (err, result_store) => {
-    // console.log(result)
-    if(result_store == null) {
-      var files = fs.readdirSync('./public/resource');
-      //console.log(files)
-      var image_files = []
-      for(let i = 0; i < files.length; ++ i) {
-        image_files.push({"file_name": files[i], "portfolio_author": false})
-      }
-      const model = {
-        search: true,
-        search_path: "search_portfolio",
-        portfolio: true,
-        portfolio_author: false,
-        login_status: false,
-        images: image_files
-      }
-      response.render("index.hbs", model)
-    } else {
-      if(result_store.data == "login") {
-        // console.log(result)
-        var files = fs.readdirSync('./public/resource');
-        //console.log(files)
-        var image_files = []
-        var flag = false;
-        if(result_store.username == "liho21ia") {
-          flag = true;
-        }
-        for(let i = 0; i < files.length; ++ i) {
-          image_files.push({"file_name": files[i], "portfolio_author": flag})
-        }
-        const model = {
-          search: true,
-          search_path: "search_portfolio",
-          portfolio: true,
-          portfolio_author: flag,
-          login_status: true,
-          images: image_files
-        }
-        response.render("index.hbs", model)
-
-    }
-}})
-})
-
-app.post('/delete_portfolio', function(request, response) {
-  console.log(request.body.delete_portfolio_path)
-
-  fs.unlinkSync(`${__dirname}/public/resource/${request.body.delete_portfolio_path}`); // __dirname：当前文件所在目录
-  response.redirect("/portfolio")
-})
-
-app.post("/upload", upload.single("file"), (request, response) => {
-  // console.log("upload file")
-  response.redirect("/portfolio")
-})
-
-app.get('/index', function(request, response) {
-  // console.log(request.sessionID)
-  var store = new RedisStore({ client })
-  store.get(request.sessionID, (err, result_store) => {
-    // console.log(result)
-    if(result_store == null) {
-      CrudAPI.get_blog_all((err, result) => {
-        if(err) {
-          response.redirect("/index")
-        }
-        result_post = []
-        for(let i = 0; i < result.length; ++ i) {
-          result[i].createTime = transformTime(result[i].createTime)
-          // result[i].title = result[i].createTime + "   " + result[i].title
-          result_post.push({"id": result[i].id, "title": result[i].title, "createTime": result[i].createTime, "author": false})
-        }
-        const model = {
-          search: true,
-          search_path: "search",
-          blog: true,
-          author: false,
-          login_status: false,
-          Blogs: result_post
-        }
-        response.render("index.hbs", model)
-      })
-    } else {
-      CrudAPI.get_blog_all((err, result) => {
-        if(err) {
-          response.redirect("/index")
-        }
-        result_post = []
-        var flag = false;
-        if(result_store.username == "liho21ia") {
-          flag = true;
-        }
-        for(let i = 0; i < result.length; ++ i) {
-          result[i].createTime = transformTime(result[i].createTime)
-          // result[i].title = result[i].createTime + "   " + result[i].title
-          result_post.push({"id": result[i].id, "title": result[i].title, "createTime": result[i].createTime, "author": flag})
-        }
-        // console.log(result_store)
-        
-        // console.log(flag)
-        const model = {
-          search: true,
-          search_path: "search",
-          blog: true,
-          author: flag,
-          login_status: true,
-          Blogs: result_post
-        }
-        response.render("index.hbs", model)
-      })
-    }
-  })
-})
-
-app.post('/search', function(request, response) {
-
-  var store = new RedisStore({ client })
-  store.get(request.sessionID, (err, result_store) => {
-    // console.log(result)
-    if(result_store == null) {
-      CrudAPI.get_blog_like(request.body.search, (err, result) => {
-        if(err) {
-          response.redirect("/index")
-        }
-        
-        for(let i = 0; i < result.length; ++ i) {
-          result[i].createTime = transformTime(result[i].createTime)
-          // result[i].title = result[i].createTime + "   " + result[i].title
-        }
-        //console.log(result)
-        const model = {
-          search: true,
-          search_path: "search",
-          blog: true,
-          login_status: false,
-          Blogs: result
-        }
-        response.render("index.hbs", model)
-      })
-    } else {
-      CrudAPI.get_blog_like(request.body.search, (err, result) => {
-        if(err) {
-          response.redirect("/index")
-        }
-        
-        for(let i = 0; i < result.length; ++ i) {
-          result[i].createTime = transformTime(result[i].createTime)
-          // result[i].title = result[i].createTime + "   " + result[i].title
-        }
-        //console.log(result)
-        const model = {
-          search: true,
-          search_path: "search",
-          blog: true,
-          login_status: true,
-          Blogs: result
-        }
-        response.render("index.hbs", model)
-      })
-    }
-
-  })
-    
-})
-
-
-app.post('/search_portfolio', function(request, response) {
-  var store = new RedisStore({ client })
-  store.get(request.sessionID, (err, result_store) => {
-    // console.log(result)
-    if(result_store == null) {
-      var files = fs.readdirSync('./public/resource');
-      //console.log(files)
-      var image_files = []
-      for(let i = 0; i < files.length; ++ i) {
-        if(files[i] != request.body.search) {
-          continue
-        }
-        image_files.push({"file_name": files[i]})
-      }
-      const model = {
-        search: true,
-        search_path: "search_portfolio",
-        portfolio: true,
-        portfolio_author: false,
-        login_status: false,
-        images: image_files
-      }
-      response.render("index.hbs", model)
-    } else {
-      if(result_store.data == "login") {
-        // console.log(result)
-        var files = fs.readdirSync('./public/resource');
-        //console.log(files)
-        var image_files = []
-        var flag = false;
-        if(result_store.username == "liho21ia") {
-          flag = true;
-        }
-        for(let i = 0; i < files.length; ++ i) {
-          if(files[i] != request.body.search) {
-            continue
-          }
-          image_files.push({"file_name": files[i]})
-        }
-        const model = {
-          search: true,
-          search_path: "search_portfolio",
-          portfolio: true,
-          portfolio_author: flag,
-          images: image_files,
-          login_status: true,
-        }
-        response.render("index.hbs", model)
-
-    }
-}})
-})
-
-app.post('/search_faq', function(request, response) {
-  // console.log(request.body.search)
-  var store = new RedisStore({ client })
-  store.get(request.sessionID, (err, result_store) => {
-    // console.log(result)
-    if(result_store == null) {
-      CrudAPI.get_faq_like(request.body.search, (err, result) => {
-        if(err) {
-          // console.log(err)
-          response.redirect("/index")
-        } else {
-
-          // console.log(result)
-          result_post = []
-          for(let i = 0; i < result.length; ++ i) {
-            result_post.push({"id": result[i].id, "question": result[i].question, "answer": result[i].answer, "author": false})
-          }
-          // console.log(result)
-          const model = {
-            search: true,
-            search_path: "search_faq",
-            faq: true,
-            FAQ: result_post,
-            login_status: false,
-          }
-          response.render("index.hbs", model)
-        }
-      })
-    } else {
-      CrudAPI.get_faq_like(request.body.search, (err, result) => {
-        if(err) {
-          // console.log(err)
-          response.redirect("/index")
-        } else {
-
-          // console.log(result)
-          result_post = []
-          for(let i = 0; i < result.length; ++ i) {
-            result_post.push({"id": result[i].id, "question": result[i].question, "answer": result[i].answer, "author": false})
-          }
-          // console.log(result)
-          const model = {
-            search: true,
-            search_path: "search_faq",
-            faq: true,
-            FAQ: result_post,
-            login_status: true,
-          }
-          response.render("index.hbs", model)
-        }
-      })
-    }
-
-})
-      
-})
-
-
-app.get('/blog/:id', function(request, response){
-  var store = new RedisStore({ client })
-  store.get(request.sessionID, (err, result_store) => {
-    // console.log(result)
-    if(result_store == null) {
-      const id = request.params.id
-      // console.log(id)
-      CrudAPI.get_blog(id, (err, result) => {
-        if(err) {
-          response.redirect("/index")
-        }
-        
-        // console.log(result)
-        const model = {
-          search: false,
-          blogpage: true,
-          title: result.title,
-          login_status: false,
-          content: result.content
-        }
-        response.render("index.hbs", model)
-      })
-    } else {
-      const id = request.params.id
-      // console.log(id)
-      CrudAPI.get_blog(id, (err, result) => {
-        if(err) {
-          response.redirect("/index")
-        }
-        
-        // console.log(result)
-        const model = {
-          search: false,
-          blogpage: true,
-          title: result.title,
-          login_status: true,
-          content: result.content
-        }
-        response.render("index.hbs", model)
-      })
-    }
-
-  })
-
-})
-
-app.get('/login', function(request, response) {
-  response.render("login.hbs")
-})
-
-app.get('/add_blog', function(request, response) {
-  var store = new RedisStore({ client })
-  store.get(request.sessionID, (err, result) => {
-    // console.log(result)
-    if(result == null) {
-      response.redirect("/login")
-    } else {
-      if(result.data == "login") {
-        response.render("blog_operation.hbs")
-      } else response.redirect("/login")
-    }
-  })
-})
-
-app.post('/add_blog', function(request, response) {
-  // console.log(request.body.title, request.body.content)
-  CrudAPI.add_blog(request.body.title, request.body.content, (err, data) => {
-    if(err) {
-      console.log("add blog failed")
-      response.redirect("/add_blog")
-    } else {
-      response.redirect("/add_blog")
-    }
-  })
-  
-})
-
-app.get("/about", function(request, response) {
-  var store = new RedisStore({ client })
-  store.get(request.sessionID, (err, result_store) => {
-    // console.log(result)
-    if(result_store == null) {
-      const model = {
-        aboutMe: true,
-        login_status: false,
-      }
-      response.render("index.hbs", model)
-    } else {
-      const model = {
-        aboutMe: true,
-        login_status: true,
-      }
-      response.render("index.hbs", model)
-    }
-
-  })
-
-})
-
-
-app.get("/contact", function(request, response) {
-  var store = new RedisStore({ client })
-  store.get(request.sessionID, (err, result_store) => {
-    // console.log(result)
-    if(result_store == null) {
-      const model = {
-        contactMe: true,
-        login_status: false,
-      }
-      response.render("index.hbs", model)
-    } else {
-      const model = {
-        contactMe: true,
-        login_status: true,
-      }
-      response.render("index.hbs", model)
-    }
-
-  })
-
-})
-
-
-
-app.get("/freq_ask_question", function(request, response) {
-  var store = new RedisStore({ client })
-  store.get(request.sessionID, (err, result_store) => {
-    // console.log(result)
-    if(result_store == null) {
-      CrudAPI.get_faq_all((err, result) => {
-        if(err) {
-          response.redirect("/freq_ask_question")
-        }
-        result_post = []
-        for(let i = 0; i < result.length; ++ i) {
-          result_post.push({"id": result[i].id, "question": result[i].question, "answer": result[i].answer, "author": false})
-        }
-        // console.log(result)
-        const model = {
-          search: true,
-          search_path: "search_faq",
-          faq: true,
-          faq_author: false,
-          login_status: false,
-          FAQ: result_post
-        }
-        response.render("index.hbs", model)
-      })
-    } else {
-      CrudAPI.get_faq_all((err, result) => {
-        if(err) {
-          response.redirect("/freq_ask_question")
-        }
-        result_post = []
-        var flag = false;
-        if(result_store.username == "liho21ia") {
-          flag = true;
-        }
-        for(let i = 0; i < result.length; ++ i) {
-          result_post.push({"id": result[i].id, "question": result[i].question, "answer": result[i].answer, "author": flag})
-        }
-        // console.log(result)
-        const model = {
-          search: true,
-          search_path: "search_faq",
-          faq: true,
-          faq_author: flag,
-          login_status: true,
-          FAQ: result_post
-        }
-        response.render("index.hbs", model)
-      })
-    }
-  })
-})
-
-
-app.get('/register', function(request, response) {
-  response.render("register.hbs")
-})
 
 app.get("/logout", function(request, response) {
   var store = new RedisStore({ client })
@@ -578,15 +51,15 @@ app.post('/login', function(request, response) {
   CrudAPI.get_hashed_by_name(request.body.username, (err, data) => {
     if(err) {
       console.log("get hashed password failed")
-      response.render("login.hbs")
+      return response.send("database error")
     } else {
       // console.log(data)
       const hash = data.password_hash
       const result = bcrypt.compareSync(request.body.passwd, hash);
       if(result == false) {
-        response.render("login.hbs")
+        return response.send("password not correct")
       }
-      else response.redirect("/index")
+      return response.send("ok")
     }
   })
   // console.log(request.sessionID)
@@ -604,33 +77,35 @@ app.post('/login', function(request, response) {
 })
 
 
-app.post("/add_faq", function(request, response) {
-  CrudAPI.add_faq(request.body.question, request.body.answer, (err, data) => {
-    if(err) {
-      console.log("add freq_ans_question failed!")
-    }
-  })
-  response.redirect("/freq_ask_question")
+app.post("/ask_chatgpt", function(req, response) {
+  // console.log(req.body)
+  var options = {
+    'method': 'POST',
+    'url': 'https://api.openai.com/v1/completions',
+    'headers': {
+      'Authorization': 'Bearer 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',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "model": "text-davinci-003",
+      "prompt": req.body.text,
+      "temperature": 0.5,
+      "max_tokens": 1024
+    })
+  
+  };
+  request(options, function (error, chatgpt_response) {
+    if (error) throw new Error(error);
+    console.log(chatgpt_response.body);
+    return response.json(chatgpt_response.body)
+  });
+
+
+  // response.json({"id":"cmpl-6fAA1f9BPBrOwEFYkmHSKMIjXt6mX","object":"text_completion","created":1675268525,"model":"text-davinci-003","choices":[{"text":"\n\n多目标追踪（Multi-Object Tracking，MOT）是","index":0,"logprobs":null,"finish_reason":"length"}],"usage":{"prompt_tokens":16,"completion_tokens":29,"total_tokens":45}})
+  
 })
 
-app.post("/delete_faq", function(request, response) {
-  const number = parseInt(request.body.delete_faq_id)
-  CrudAPI.delete_faq(number)
-  response.redirect("/freq_ask_question")
-})
 
-app.post("/update_faq", function(request, response) {
-  const number = parseInt(request.body.update_faq_id)
-  CrudAPI.update_faq(request.body.answer_update, number)
-  response.redirect("/freq_ask_question")
-})
-
-app.post("/delete_blog", function(request, response) {
-  const number = parseInt(request.body.delete_blog_id)
-  // console.log(number)
-  CrudAPI.delete_blog(number)
-  response.redirect("/index")
-})
 
 app.post("/register", function(request, response) {
   const saltRounds = 10
@@ -642,7 +117,7 @@ app.post("/register", function(request, response) {
     }
     console.log("register success, now you can login");
   })
-  response.redirect("/login")
+  return response.send("ok")
 })
 
 app.listen(process.env.PORT || 8080)
